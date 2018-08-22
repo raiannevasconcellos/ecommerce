d50424a78679b48eb63b06c925afa33a6c666ff6
Raianne Vasconcellos <raianne_vasconcellos@hotmail.com>
2018-08-22 14:41:35 -0300

    Tela de login

---
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -0,0 +1,49 @@
+<?php 
+
+namespace Hcode;
+
+class Model {
+
+	private $values = [];
+
+	public function __call($name, $args){
+			// pra saber se foi o método SET ou GET utilizado.. então eu peço pra ele me retornar as 3 primeiras letras da palavra
+
+		$method = substr ($name, 0, 3); 	// aqui quer dizer, a partir da posição 0 traga 0,1,2.
+
+		$fieldName = substr($name, 3, strlen($name));
+
+		switch ($method) {
+			case "get":
+				return $this->values[$fieldName];
+			break;
+
+			case "set":
+				$this->values[$fieldName] = $args[0];
+			break;
+			
+		}
+
+	}
+
+	public function setData($data=array()){
+
+		foreach ($data as $key => $value) {
+			
+			$this->{"set".$key}	($value);	//coloca-se dentro de chaves pra deixar dinâmico e eu conseguir concatenar a palavra set com a key que virá do bando de dados [pode ser idusuario, idlogin, etc]
+
+		}
+	}
+
+	public function getValues (){
+
+		return $this->values;
+
+	}
+
+
+}
+
+
+
+ ?>
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,13 +9,15 @@ class Page{
 	private $tpl;
 	private $options = [];
 	private $defaults = [
-
+		"header"=>true,
+		"footer"=>true,
 		"data"=> []
 
 	];
 
 	public function __construct($opts = array(), $tpl_dir = "/views/"){
 
+		//$this->defaults["data"]["session"] = $_SESSION;
 		$this->options = array_merge($this->defaults, $opts); 	// o 2º param. sobreescreve o primeiro.. quero que o que a pessoa informe no construct sobreescreva o default.
 
 
@@ -31,7 +33,7 @@ class Page{
 
 		$this-> setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if ($this->options["header"] === true) $this->tpl->draw("header");
 
 	}
 
@@ -53,7 +55,7 @@ class Page{
 
 	public function __destruct(){
 
-		$this->tpl->draw("footer");
+		if ($this->options["footer"] === true) $this->tpl->draw("footer");
 
 	}
 
