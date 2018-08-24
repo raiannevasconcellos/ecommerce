54f1e16165ad454788071ce865b1d080b9cbd3cc
Raianne Vasconcellos <raianne_vasconcellos@hotmail.com>
2018-08-24 11:24:41 -0300

    Implementando recuperação de senha

---
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -0,0 +1,76 @@
+<?php 
+
+namespace Hcode;
+
+use Rain\Tpl;
+
+class Mailer {
+
+	const USERNAME = "cursodephp7@gmail.com";		// criei esse email e a senha [php7bolado]
+
+	const PASSWORD = "<?password?>";
+
+	const NAME_FROM = "Hcode Store";
+
+	private $mail;
+
+
+	public function __construct($toAddress, $toName, $subject, $tplName, $data = array()){
+
+		$config = array(
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/email/",
+			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+			"debug"         => false // set to false to improve the speed
+			);
+
+		Tpl::configure( $config );
+
+		$tpl = new Tpl;
+
+		foreach ($data as $key => $value) {
+			$this->tpl->assign($key, $value);
+		}
+
+		$html = $tpl->draw($tplName, true);
+
+		$this->mail = new \PHPMailer;
+
+		$this->mail->isSMTP();
+
+		$this->mail ->SMTPDebug = 0;
+
+		$this->mail->Debugoutput = 'html';
+
+		$this->mail->Host = 'smtp.gmail.com';
+
+		$this->mail->Port = 587;
+
+		$this->mail->SMTPSecure = 'tls';
+
+		$this->mail->SMTPAuth = true;
+
+		$this->mail->Username = Mailer::USERNAME;
+
+		$this->mail->Password = Mailer::PASSWORD;
+
+		$this->mail->setFrom(Mailer::USERNAME, Mailer::NAME_FROM);
+
+		$this->mail->addAdress($toAddress, $toName);
+
+		$this->mail->Subject = $subject;
+
+		$this->mail->msgHTML($html);
+
+		$this->mail->AltBody = 'This is a plain-text message body';
+	
+	}
+
+
+	public function send(){
+
+
+		return $this->mail->send();
+	}
+}
+
+ ?>
